c4d4e891422d7eb3dbd3c1e8d7c98b012aa6e7d4
MUHAMMAD AHMAD RAZA <160361921+mahmadraza172@users.noreply.github.com>
2024-03-03 16:22:23 +0500

    Add files via upload
    In this program

---
+++ biodata.js
@@ -0,0 +1,12 @@
+var name1 = 'Muhammad Ahmad Raza';
+console.log(name1);
+var name2 = 'Raza';
+console.log(name2);
+var age = '18';
+console.log(age);
+var address = 'Block NO136 House No6 Majeed SRE';
+console.log(address);
+var education = 'Inter';
+console.log(education);
+var height = '5.11';
+console.log(height);
